03f99bed80db6a152fd453f8b4389dc63ede7271
Jason Geller <jasongeller@mdjgps405mcgu.bc.edu>
2025-03-12 14:29:55 -0400

    add paper

---
+++ create_env_dev.R
@@ -0,0 +1,22 @@
+required_packages <- c(
+  "easystats", "tidyverse", "knitr", "cowplot", "ggeffects", "here", 
+  "data.table", "ggrepel", "brms", "ggdist", "emmeans", "tidylog", 
+  "tidybayes", "hypr", "colorspace", "ragg", "ggtext", "MetBrewer", 
+  "modelbased", "flextable", "posterior", "tinytable", "ggokabeito",
+  "patchwork"
+)
+
+rix(date = "2025-03-03",
+    r_pkgs = required_packages, 
+    system_pkgs = c("quarto", "git", "pandoc", "cmdstan", "chromium"),
+    git_pkgs = list(
+      list(
+        package_name = "cmdstanr",
+        repo_url = "https://github.com/stan-dev/cmdstanr",
+        commit = "79d37792d8e4ffcf3cf721b8d7ee4316a1234b0c")
+      ), 
+    tex_pkgs = c("amsmath", "ninecolors", "apa7", "scalerel", "threeparttable", "threeparttablex", "endfloat", "environ", "multirow", "tcolorbox", "pdfcol", "tikzfill", "fontawesome5", "framed", "newtx", "fontaxes", "xstring", "wrapfig", "tabularray", "siunitx", 
+                 "fvextra", "geometry","setspace", "fancyvrb", "anyfontsize"), 
+    ide = "rstudio",
+    project_path = ".",
+    overwrite = TRUE)
