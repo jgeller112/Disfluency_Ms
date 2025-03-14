4e59e8ca36623b3e05f848812c0675626c9a41a4
jgeller112 <jason-geller@uiowa.edu>
2025-03-13 20:44:06 -0400

    update readme

--- create_env_dev.R
+++ create_env_dev.R
@@ -8,15 +8,15 @@ required_packages <- c(
 
 rix(date = "2025-03-03",
     r_pkgs = required_packages, 
-    system_pkgs = c("quarto", "git", "pandoc", "cmdstan", "chromium"),
+    system_pkgs = c("quarto", "git", "pandoc", "cmdstan", "nix"),
     git_pkgs = list(
       list(
         package_name = "cmdstanr",
         repo_url = "https://github.com/stan-dev/cmdstanr",
-        commit = "79d37792d8e4ffcf3cf721b8d7ee4316a1234b0c")
+        commit = "12211d4b35605d416d6e22078db81cfe9e5a64f5")
       ), 
     tex_pkgs = c("amsmath", "ninecolors", "apa7", "scalerel", "threeparttable", "threeparttablex", "endfloat", "environ", "multirow", "tcolorbox", "pdfcol", "tikzfill", "fontawesome5", "framed", "newtx", "fontaxes", "xstring", "wrapfig", "tabularray", "siunitx", 
                  "fvextra", "geometry","setspace", "fancyvrb", "anyfontsize"), 
-    ide = "rstudio",
+    ide = "positron",
     project_path = ".",
     overwrite = TRUE)
